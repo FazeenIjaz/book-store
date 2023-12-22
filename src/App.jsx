be0d29f839ed8f889ec9e62c0797aa134214d493
Fazeen <fazeenijaz614@gmail.com>
2023-12-22 19:58:15 +0500

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+import Services from "./components/Services/Services.jsx";
+import Banner from "./components/Banner/Banner.jsx";
+import AppStore from "./components/AppStore/AppStore.jsx";
+import Testimonial from "./components/Testimonial/Testimonial.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import OrderPopup from "./components/OrderPopup/OrderPopup.jsx";
+import Books from "./components/BooksSlider/Books.jsx";
+import PdfReader from "./components/PdfReader/PdfReader.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [orderPopup, setOrderPopup] =useState(false);
+
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  };
+
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+      <Navbar handleOrderPopup={handleOrderPopup} />
+      <Hero handleOrderPopup={handleOrderPopup} />
+      <Services handleOrderPopup={handleOrderPopup} />
+      <Banner />
+      <AppStore />
+      {/* <PdfReader /> */}
+      <Books />
+      <Testimonial />
+      <Footer />
+      <OrderPopup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/AppStoreBanner/AppStoreBanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AppStoreBanner = () => {
+  return (
+    <div>
+      app store
+    </div>
+  )
+}
+
+export default AppStoreBanner
